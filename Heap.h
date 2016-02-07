faa636884d706e0998c5e1e73b93cd32f1ae8956
wkzhuang <jameszwk9596@gmail.com>
2016-02-07 01:17:53 -0800

    modify Heap.h

--- Heap.h
+++ Heap.h
@@ -2,7 +2,23 @@
 #ifndef HEAP_H_
 #define HEAP_H_
 
+#include <iostream>
+#include "freqCounter.h"
+using namespace std;
+
 class Heap{
+
+private:
+	int occupancy;
+	pair<char,int>* myHeap;
+
+public:
+	Heap();
+	~Heap();
+	//void buildArray(sting filename);
+	void minHeapify(pair<char,int>* arrayOfNodes, int size);
+	pair<char,int> deleteMin();
+	// void print();
 };
 
 #endif
